d64d8263af16e3fce42d419e721716b695a82a85
Shishir <shiishir@gmail.com>
2021-08-16 12:02:05 +0600

    Update deposit and account balance

--- js/banking.js
+++ js/banking.js
@@ -2,11 +2,24 @@
 document.getElementById('deposit-button').addEventListener('click', function () {
     /* Get the amount deposited */
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const newDepositAmountText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositAmountText);
 
+    /* Update deposit total */
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount;
+    const previousDepositAmountText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositAmountText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+
+    depositTotal.innerText = newDepositTotal;
     /* Clear the deposit input field */
     depositInput.value = '';
+
+    /* Update account balance */
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
 })
+
